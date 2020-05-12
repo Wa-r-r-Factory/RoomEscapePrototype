6088a191b83c365884f8ffde04771f186e0be888
juninjune <r0otstat1c@naver.com>
2020-05-13 00:27:18 +0900

    5/11 Update

---
+++ Assets/Scripts/ButtonController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonController : MonoBehaviour
+{
+    public GameObject Video;
+
+    private void Start()
+    {
+        Video.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if(Physics.Raycast(ray, out hit, 10)){
+                GameObject o = hit.transform.gameObject;
+
+                if(o == gameObject)
+                {
+                    Video.SetActive(true);
+                }
+            }
+        }
+    }
+}
--- Assets/Scripts/MediaBoard.cs
+++ Assets/Scripts/MediaBoard.cs
@@ -6,61 +6,10 @@ using UnityEngine.Video;
 
 public class MediaBoard : MonoBehaviour {
 
-	public GameObject[] poster, textCanvas, videoPanel;
-	public VideoPlayer vPlayer;
+	public GameObject vPlayer;
 
-	void Start () 
-	{
-		/**vPlayer.Stop();
-		foreach (GameObject p in poster)
-		   p.SetActive (false);
-
-		foreach (GameObject t in textCanvas)
-		   t.SetActive (false);
-
-		foreach (GameObject v in videoPanel)
-		   v.SetActive (true);**/
-
-		vPlayer.Play();
-        /*
-        foreach (GameObject p in poster)
-			p.SetActive(true);
-
-		foreach (GameObject t in textCanvas)
-			t.SetActive(true);
-        **/
-
-	}
-
-    /**void OnTriggerEnter(Collider other)
-	{
-		if (other.tag == "Player")
-		{
-			vPlayer.Play();
-		    foreach (GameObject p in poster)
-		        p.SetActive (true);
-
-		    foreach (GameObject t in textCanvas)
-		        t.SetActive (true);
-
-		    foreach (GameObject v in videoPanel)
-		        v.SetActive (false);
-		}
-	}**/
-
-	/**void OnTriggerExit(Collider other)
-	{
-		if (other.tag == "Player")
-		{
-			foreach (GameObject p in poster)
-		        p.SetActive (false);
-
-		    foreach (GameObject t in textCanvas)
-		        t.SetActive (false);
-
-		    foreach (GameObject v in videoPanel)
-		        v.SetActive (true);
-			vPlayer.Stop();
-		}
-	}**/
+	public void PlayVideo()
+    {
+        vPlayer.SetActive(true);
+    }
 }
